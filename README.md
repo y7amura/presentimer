98ed526026a646069ddb95625de2bc5c57a4cf8e
tamy <tamura.yasumasa@gmail.com>
2021-02-04 18:00:38 +0900

    add alarm function

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Presentation timer.
 
-**This is vaporware! Currently this only supports lap recording.**
+**This is still vaporware! Not tested so well.**
+
+## Features
+- You can set at most three alarms with favorite colors.
+- You can easily record lap time, and copy it to the clipboard.
 
 ## License
 The source code is licensed MIT.
--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// variables
-let timer;
-let elapsedTime = 0;
-let lastLap = [];
-
 // components
+const clock = document.getElementById("time");
 const startButton = document.getElementById("startButton");
 const pauseButton = document.getElementById("pauseButton");
 const lapButton = document.getElementById("lapButton");
@@ -11,6 +7,26 @@ const stopButton = document.getElementById("stopButton");
 const resetButton = document.getElementById("resetButton");
 const saveButton = document.getElementById("saveButton");
 const lapField = document.getElementById("lapField");
+const alarmEnablers = [
+  document.getElementById("alarm-enabler-1"),
+  document.getElementById("alarm-enabler-2"),
+  document.getElementById("alarm-enabler-3"),
+];
+const alarmMins = [
+  document.getElementById("alarm-min-1"),
+  document.getElementById("alarm-min-2"),
+  document.getElementById("alarm-min-3"),
+];
+const alarmSecs = [
+  document.getElementById("alarm-sec-1"),
+  document.getElementById("alarm-sec-2"),
+  document.getElementById("alarm-sec-3"),
+];
+const alarmColors = [
+  document.getElementById("alarm-color-1"),
+  document.getElementById("alarm-color-2"),
+  document.getElementById("alarm-color-3"),
+];
 
 
 const parseTime = (time) => {
@@ -29,15 +45,20 @@ const format = (time) => {
 };
 
 const updateDisplay = (time) => {
-  document.getElementById("time").innerHTML = format(time);
+  clock.innerHTML = format(time);
 };
 
 
-// Timer controllers
+// Timer
+let timer;
+let elapsedTime = 0;
+let lastLap = [];
+
 const startTimer = () => {
   const startAt = Date.now() - elapsedTime;
   timer = setInterval(() => {
     elapsedTime = Date.now() - startAt;
+    checkAlarm(elapsedTime);
     updateDisplay(elapsedTime);
   }, 1000);
 };
@@ -54,15 +75,65 @@ const resetTimer = () => {
 };
 
 
+// Alarms
+let alarms = [];
+let alarmInitialized = false;
+
+const initializeAlarm = () => {
+  if(alarmInitialized) return;
+  [0, 1, 2].filter(idx => { return alarmEnablers[idx].checked; }).forEach(idx => {
+    const t = (parseInt(alarmMins[idx].value, 10) * 60 + parseInt(alarmSecs[idx].value, 10)) * 1000;
+    alarms.push({
+      time: t,
+      color: alarmColors[idx].value,
+    });
+  });
+  alarms.sort((a, b) => {
+    if(a.time < b.time) return -1;
+    if(a.time > b.time) return 1;
+    return 0;
+  });
+  alarmInitialized = true;
+};
+
+const checkAlarm = (time) => {
+  alarms.forEach(alarm => {
+    if(time >= alarm.time) {
+      clock.style.color = alarm.color;
+    }
+  });
+};
+
+const resetAlarms = () => {
+  alarmInitialized = false;
+  alarms = [];
+  clock.style.color = "#fafafa";
+};
+
+const toggleAlarm = (idx) => {
+  return () => {
+    const flag = !alarmEnablers[idx].checked;
+    alarmMins[idx].disabled = flag;
+    alarmSecs[idx].disabled = flag;
+    alarmColors[idx].disabled = flag;
+  };
+};
+
+
 // Actions
 const start = () => {
   startTimer();
+  initializeAlarm();
   startButton.style.display = "none";
   pauseButton.style.display = "inline";
   lapButton.disabled = false;
   stopButton.style.display = "inline";
   resetButton.style.display = "none";
   stopButton.disabled = false;
+  alarmEnablers.forEach(enabler => { enabler.disabled = true; });
+  alarmMins.forEach(alarmMin => { alarmMin.disabled = true; });
+  alarmSecs.forEach(alarmSec => { alarmSec.disabled = true; });
+  alarmColors.forEach(alarmColor => {alarmColor.disabled = true; });
 };
 
 const pause = () => {
@@ -96,9 +167,14 @@ const stop = () => {
 const reset = () => {
   if(!confirm("Lap record will be discarded. OK?")) return;
   resetTimer();
+  resetAlarms();
   startButton.disabled = false;
   resetButton.disabled = true;
   lapField.value = "";
+  alarmEnablers.forEach(enabler => { enabler.disabled = false; });
+  alarmMins.forEach(alarmMin => { alarmMin.disabled = false; });
+  alarmSecs.forEach(alarmSec => { alarmSec.disabled = false; });
+  alarmColors.forEach(alarmColor => {alarmColor.disabled = false; });
 };
 
 const save = () => {
@@ -115,3 +191,28 @@ lapButton.addEventListener("click", lap);
 stopButton.addEventListener("click", stop);
 resetButton.addEventListener("click", reset);
 saveButton.addEventListener("click", save);
+alarmEnablers.forEach((alarmEnabler, idx) => {
+  alarmEnabler.addEventListener("click", toggleAlarm(idx));
+});
+
+const alarmTab = document.getElementById("alarm-tab");
+const alarmPanel = document.getElementById("alarm-panel");
+const lapfieldTab = document.getElementById("lapfield-tab");
+const lapfieldPanel = document.getElementById("lapfield-panel");
+
+alarmTab.addEventListener("click", () => {
+  alarmTab.classList.add("w3-green");
+  alarmTab.classList.remove("w3-gray");
+  alarmPanel.classList.remove("w3-hide-small");
+  lapfieldTab.classList.remove("w3-green");
+  lapfieldTab.classList.add("w3-gray");
+  lapfieldPanel.classList.add("w3-hide-small");
+});
+lapfieldTab.addEventListener("click", () => {
+  lapfieldTab.classList.add("w3-green");
+  lapfieldTab.classList.remove("w3-gray");
+  lapfieldPanel.classList.remove("w3-hide-small");
+  alarmTab.classList.remove("w3-green");
+  alarmTab.classList.add("w3-gray");
+  alarmPanel.classList.add("w3-hide-small");
+});
